934823542326f393c887db9c0af182531c03a08b
Angela Holden <angelajholden@gmail.com>
2024-04-23 18:40:10 -0500

    rest api for products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,35 +1,50 @@
 const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
 
 // @desc: Get all products from the database
 // @route: GET /api/products
 // @access: Public
 const getProducts = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: "Get Products" });
+	const products = await Product.find({});
+	res.status(200).json(products);
 });
 
 // @desc: Set a product in the database
 // @route: POST /api/products
 // @access: Public
 const setProduct = asyncHandler(async (req, res) => {
-	if (!req.body.text) {
+	if (!req.body.name) {
 		res.status(400);
 		throw new Error("Please provide a product");
 	}
-	res.status(200).json({ message: "Set Product" });
+	const product = await Product.create({ name: req.body.name });
+	res.status(200).json(product);
 });
 
 // @desc: Update a product in the database
 // @route: PUT /api/products
 // @access: Public
 const updateProduct = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Update product ${req.params.id}` });
+	const product = await Product.findById(req.params.id);
+	if (!product) {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+	const updatedProdcut = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+	res.status(200).json(updatedProdcut);
 });
 
 // @desc: Delete a product in the database
 // @route: DELETE /api/products
 // @access: Public
 const deleteProduct = asyncHandler(async (req, res) => {
-	res.status(200).json({ message: `Delete product ${req.params.id}` });
+	const product = await Product.findById(req.params.id);
+	if (!product) {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+	await Product.deleteOne({ _id: req.params.id });
+	res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getProducts, setProduct, updateProduct, deleteProduct };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-console.log("Server is running...");
-
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 3000;
+
+connectDB();
 
 const app = express();
 
